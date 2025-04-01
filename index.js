c7fcc4bfe9b67ade528a78c6e02ea30ce44ed412
ArpanK861 <arpan02kundu@gmail.com>
2025-04-01 07:37:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello MCKV who cares')
+  res.send('Hello MCKV from Arpan Kundu')
 })
 
 app.get('/courses', function (req, res) {
